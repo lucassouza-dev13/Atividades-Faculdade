14dc30d8b94a5cefeca50c6dea26df55fb575131
lucassouza-dev13 <la1765035@gmail.com>
2026-06-08 14:53:06 -0300

    removendo arquivos da raiz

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CadastroAluno.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
